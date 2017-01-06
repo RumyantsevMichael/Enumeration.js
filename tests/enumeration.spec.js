dd99f2a16e9ec3a8d0b6e6c13a86bc6cd8cf2309
Michael Rumyantsev <Rumyantsev.Michael@gmail.com>
2017-01-07 01:06:30 +0300

    Fixed module exports and added supersimple tests

---
+++ tests/enumeration.spec.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Enumeration = require('../lib/enumeration.js');
+
+const SUIT = Enumeration.create({
+    CLUBS:    1,
+    DIAMONDS: 2,
+    HEARTS:   3,
+    SPADES:   4
+});
+
+console.assert(SUIT.CLUBS instanceof SUIT);
+console.assert(SUIT.getByKey('DIAMONDS') === SUIT.DIAMONDS);
+console.assert(SUIT.getByValue(3) === SUIT.HEARTS);
+console.assert(Number(SUIT.SPADES) === 4);
+console.assert(String(SUIT.SPADES) === 'SPADES');
+
+const COLOR = Enumeration.create({
+    RED:   1,
+    GREEN: 2,
+    BLUE:  4
+});
+
+console.assert(SUIT.SPADES !== COLOR.BLUE);
